5c3a083b716bbe9564d09f592c5b9c0c4fb421a1
aslink87 <adam.slinkman@gmail.com>
2022-06-28 13:03:58 -0400

    feat: configure storybook with scss and added template component

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,13 +1,24 @@
 module.exports = {
   "stories": [
     "../**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../**/*.stories.@(js|jsx|ts|tsx)"
   ],
   staticDirs: ['../public'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    '@storybook/preset-scss'
+    /*
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+    */
   ],
   "framework": "@storybook/react",
   "core": {
