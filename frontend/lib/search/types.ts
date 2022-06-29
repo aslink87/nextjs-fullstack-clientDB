f97355be445e1e73cb53e9cc0512e1c9e23bb74a
aslink87 <adam.slinkman@gmail.com>
2022-06-29 17:10:56 -0400

    feat: added search api

---
+++ frontend/lib/search/types.ts
@@ -0,0 +1,12 @@
+export interface ISearchData {
+  "data": [
+    {
+      "id": number,
+      "attributes": {
+        "birthdate": "string",
+        "lastname": "string",
+        "firstname": "string"
+      }
+    }
+  ]
+}
---
+++ frontend/pages/api/searchDB.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ISearchData } from '../../lib/search/types';
+
+interface IApiSearchRequest extends NextApiRequest {
+  body: { searchTerm?: string };
+}
+
+export type IApiSearchResponseData = ISearchData[];
+
+
+export default async function handler(
+  req: IApiSearchRequest,
+  res: NextApiResponse<IApiSearchResponseData>
+) {
+  const {
+    body: { searchTerm },
+  } = req;
+  /*
+  const response = await fetch(`http://localhost:1337/api/individuals?filters[lastname][$eq]=${searchTerm}`)
+  const json = await response.json()
+  const data = json.data
+  console.log(data)
+  */
+
+  if (req.method === 'GET' && searchTerm && searchTerm.length > 0) {
+    const cleanedSearchTerm = searchTerm.toLowerCase().trim()
+    const response = await fetch(`http://localhost:1337/api/individuals?filters[lastname][$eq]=${cleanedSearchTerm}`)
+    const json = await response.json()
+    const data = json.data
+    console.log("get", data)
+
+    res.status(200).json(data)
+  } else {
+    res.status(400).json([]);
+  }
+}
