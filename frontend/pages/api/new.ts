1f9d42d52779e7ff4f73238cbb2ff5ec0a2d82c9
aslink87 <adam.slinkman@gmail.com>
2022-07-13 16:57:54 -0400

    refactor: completed api/new logic for handling POST requests from /pages/new

--- frontend/pages/api/new.ts
+++ frontend/pages/api/new.ts
@@ -8,6 +8,7 @@ interface IApiNewPost extends NextApiRequest {
 export type IApiSearchResponseData = ClientModel[];
 
 
+
 export default async function handler(
   req: IApiNewPost,
   res: NextApiResponse<IApiNewPost>
@@ -15,14 +16,10 @@ export default async function handler(
   const {
     body: { data },
   } = req;
-  /*
-  const response = await fetch(`http://localhost:1337/api/individuals?filters[lastname][$eq]=${searchTerm}`)
-  const json = await response.json()
-  const data = json.data
-  console.log(data)
-  */
-/*
-  if (req.method === 'POST' && data) {
+
+  // create new client
+  const newClient = async(action: string, household: number) => {
+    delete data.id
     const response = await fetch(`http://localhost:1337/api/individuals/`, {
       body: JSON.stringify({ data }),
       headers: {
@@ -32,44 +29,69 @@ export default async function handler(
     });
     const json = await response.json()
     const resData = json.data
-    console.log(resData)
-
-    res.status(200).json(data)
-  } else {
-    res.status(400).json(data);
-    console.log('failed', res)
+    const clientId = resData.id
+    if (action === "new"){
+      newHousehold(action, clientId)
+    } else if (action === "update") {
+      updateHousehold(household, clientId)
+    } else {
+      console.log("newclient else")
+    }
   }
-}
-*/
-    /*
-    const response = await fetch(`http://localhost:1337/api/individuals?filters[lastname][$eq]=${cleanedSearchTerm}`)
+
+  // create new household
+  const newHousehold = async(action: string, clientId: number) => {
+    delete data.id
+    const response = await fetch(`http://localhost:1337/api/households/`, {
+      body: JSON.stringify({ data }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
     const json = await response.json()
-    const data = json.data
-    console.log("get", data)
-    */
+    const resData = json.data
+    const household = resData.id
+    if (action === "new"){
+      updateHousehold(household, clientId)
+    } else {
+      return res.status(200).json(resData)
+    }
+  }
+
+  // add client to household
+  const updateHousehold = async(householdId: number, clientId: number) => {
+    const individualData = {
+      "data": {
+        "household": householdId
+      }
+    }
+    const response = await fetch(`http://localhost:1337/api/individuals/${clientId}?populate=household`, {
+      body: JSON.stringify(individualData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'PUT',
+    });
+    const json = await response.json()
+    const resData = json.data
+    console.log(resData)
+    return res.status(200).json(resData)
+  }
+
   if (req.method === 'POST' && data) {
     if (data.id != 0) {
       // new client, update household
       console.log('if')
+      const action = "update"
+      const household = data.id
+      newClient(action, household)
 
     } else if (data.id === 0) {
       // new client, new household
-      console.log('else if')
-      delete data.id
-      const response = await fetch(`http://localhost:1337/api/individuals/`, {
-        body: JSON.stringify({ data }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-      });
-      const json = await response.json()
-      const resData = json.data
-      console.log(resData)
-
-      res.status(200).json(data)
+      const action = "new"
+      const household = 0
+      newClient(action, household)
     }
   } else {console.log('else')}
-
-
 }
